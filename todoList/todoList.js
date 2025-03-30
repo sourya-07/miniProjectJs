7d5785ba9ee882f1acbae897fc23f789c7d82c6d
Sourya <souryagupta2005@gmail.com>
2025-03-30 15:11:23 +0530

    h

---
+++ todoList/todoList.js
@@ -0,0 +1,30 @@
+const todoList = [];
+
+renderTodoList();
+function renderTodoList(){
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const html = `<p>${todoList[i]}</p>`;
+        todoListHTML += html;
+    }
+    console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    console.log(name);
+    todoList.push(name); 
+    console.log(todoList);
+
+    inputElement.value = '';
+    renderTodoList(); 
+
+    
+
+}
